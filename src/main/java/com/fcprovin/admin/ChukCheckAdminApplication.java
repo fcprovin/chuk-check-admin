5b19330d6e007d6614c71ea588abcf92777810e6
discphy <dinophy@nate.com>
2023-05-23 15:14:06 +0900

    feat(add): Initial commit

---
+++ src/main/java/com/fcprovin/admin/ChukCheckAdminApplication.java
@@ -0,0 +1,13 @@
+package com.fcprovin.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChukCheckAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChukCheckAdminApplication.class, args);
+	}
+
+}
